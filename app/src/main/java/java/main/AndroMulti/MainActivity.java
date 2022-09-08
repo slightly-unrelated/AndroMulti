3e29f929de25e7360be8933f32b40ddda431dc3f
Slightly-unrelated <hiwrz9mqm03okjk9y6hc@gmail.com>
2022-09-08 20:18:29 +0800

    Initial Commit

---
+++ app/src/main/java/java/main/AndroMulti/MainActivity.java
@@ -0,0 +1,21 @@
+package java.main.AndroMulti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.main.AndroMulti.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/java/main/AndroMulti/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package java.main.AndroMulti.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
